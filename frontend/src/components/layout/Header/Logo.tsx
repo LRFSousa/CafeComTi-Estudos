ede3cbf01f1ea6368cc48caaf291bdda20fb70f3
Luis Ricardo Sousa <luisricardofs@gmail.com>
2025-08-06 17:49:36 -0300

    Ajustes NavItem

--- frontend/src/components/layout/Header/Logo.tsx
+++ frontend/src/components/layout/Header/Logo.tsx
@@ -8,18 +8,14 @@ function Logo() {
   return (
     <div className="flex items-center justify-between w-full xl:w-[280px]">
       <a href="/" className="flex items-center space-x-2">
-        <span className="hidden lg:block text-[26px] font-bold text-[#012970] font-nunito">
-          AdminDashboard
+        <span className="hidden lg:block text-[26px] font-bold text-[#012970] font-nunito pl-4 pr-4">
+          Café com TI
         </span>
         <AlignJustify
           className="w-6 h-6 text-[#012970] cursor-pointer"
           onClick={handleToggleSideBar}
         />
       </a>
-      <i
-        className="bi bi-list text-[21px] pl-2 cursor-pointer text-[#012970]"
-        onClick={handleToggleSideBar}
-      ></i>
     </div>
   );
 }
