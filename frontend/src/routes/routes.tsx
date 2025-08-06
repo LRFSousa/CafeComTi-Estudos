10924d5e6b7a4045fc38b4c8658fe62bc340a071
Luis Ricardo Sousa <luisricardofs@gmail.com>
2025-08-06 10:14:09 -0300

    Construção base de routas com react-router-dom

---
+++ frontend/src/routes/routes.tsx
@@ -0,0 +1,11 @@
+// src/AppRoutes.tsx
+import { Route, Routes } from "react-router-dom";
+import Header from "../components/layout/Header";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Header />} />
+    </Routes>
+  );
+}
